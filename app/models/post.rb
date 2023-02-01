1616343fff22e3c3796c14c35e6fe76c2afe6d99
Andrew <aforest41@yahoo.com>
2023-01-31 16:59:57 -0800

    commit tree 91d159ace5d7d7c4fb3d53cfb4d1705ed1d442db

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validates :title, format: {with: /Won't Believe|Secret|Top \d|Guess/i, message: "not clickbaity enough ser!"} #cleaner version of clickbait_y
+    #validate :clickbait_y
+
+    # def clickbait_y
+    #     if(title.is_a?(String))
+    #         if !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Top") && !title.include?("Guess")
+    #             errors.add(:title, "Not Click baity")
+    #         end
+    #     end
+    # end
+
+    # def clickbait_y
+    #     if [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i].non? {|pattern| pattern.match(self.title)}
+    #         errors.add(:title, "Not Click Baity")
+    #     end
+    # end
+
 end
